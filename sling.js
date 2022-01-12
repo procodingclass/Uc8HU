eb229f97caf494444b8d2bbdbb139643991b5074
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-01-12 21:21:37 +0530

    Add files via upload

--- sling.js
+++ sling.js
@@ -1,39 +1,33 @@
-class Sling{
-	constructor(body,anchor)
-	{
-		
-		var options={ 
-			bodyA:body,			 
-			pointB:anchor, 
-			stiffness:0.004, 
-			length:10
-			
-		}
-		
-		this.bodyA=body
-		this.pointB=anchor
-		this.launcher=Constraint.create(options)
-		World.add(world,this.launcher)
-	}
+class Sling {
+  constructor(body, anchor) {
+    var options = {
+      bodyA: body,
+      pointB: anchor,
+      stiffness: 0.004,
+      length: 10,
+    };
 
-	attach(body){
-		this.launcher.bodyA=body;
-	}
+    this.bodyA = body;
+    this.pointB = anchor;
+    this.launcher = Constraint.create(options);
+    World.add(world, this.launcher);
+  }
 
-	fly()
-	{
-		this.launcher.bodyA=null;
-	}
+  attach(body) {
+    this.launcher.bodyA = body;
+  }
 
-	display()
-	{
-		if(this.launcher.bodyA)
-		{
-			var pointA=this.bodyA.position;
-			var pointB=this.pointB
+  fly() {
+    this.launcher.bodyA = null;
+  }
 
-			strokeWeight(2);		
-			line(pointA.x,pointA.y,pointB.x,pointB.y);
-		}
-	}
+  display() {
+    if (this.launcher.bodyA) {
+      var pointA = this.bodyA.position;
+      var pointB = this.pointB;
+
+      strokeWeight(2);
+      line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+  }
 }
